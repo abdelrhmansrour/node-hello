421a4a82ce636692feb00a89d56aa691ee8234a0
abdelrhmansrour <abdelrhmansrour98@gmail.com>
2025-07-09 19:53:11 +0300

    newrelic setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,17 +10,26 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "node_hello_image" {
-  name         = "node-hello:latest"
+  name = "node-hello:latest"
+
   build {
-    context    = "../"  # Adjust path to your Dockerfile if needed
+    context = "../"
   }
 }
 
 resource "docker_container" "node_hello_container" {
   name  = "node-hello-container"
   image = docker_image.node_hello_image.name
+
   ports {
     internal = 3000
     external = 3000
+  }
+
+  
+  lifecycle {
+    replace_triggered_by = [
+      docker_image.node_hello_image
+    ]
   }
 }
